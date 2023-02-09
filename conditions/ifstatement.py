bd8b33cdf960992e87d289a2eb6aff6fbec8e619
eMobilis <sandrasasimwa@gmail.com>
2023-02-09 09:30:26 +0300

    first commit

---
+++ conditions/ifstatement.py
@@ -0,0 +1,14 @@
+dial = int(input("Enter a number"))
+if dial == 1 :
+    print("enrol bonga points")
+elif dial == 2 :
+    print("okoa jahazi")
+elif dial == 3 :
+    print("data deals")
+elif dial == 4:
+    print("minute deals")
+elif dial == 5 :
+    print("buy bundles")
+else :
+    print("safaricom twaweza")
+
---
+++ datastructures/dictionary.py
@@ -0,0 +1,5 @@
+friends = {"loui":25,"lisa":22,"rita":26}
+
+friends["marie"] = 23
+
+print(friends)
