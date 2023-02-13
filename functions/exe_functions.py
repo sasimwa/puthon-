4272d611095b85a87a2f200ae4f619dd5c0515e5
eMobilis <sandrasasimwa@gmail.com>
2023-02-13 09:02:52 +0300

    first function

---
+++ functions/exe_functions.py
@@ -0,0 +1,6 @@
+def area():
+    number1 =int(input("Enter number 1"))
+    number2 =int(input("Enter number 2"))
+    rectangle_area = number1 * number2
+    print(f"The area of the rectangle is {rectangle_area} squared cm")
+area()
