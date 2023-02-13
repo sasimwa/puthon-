91c9e436bca040d21e098e7145b90fe687c34c14
eMobilis <sandrasasimwa@gmail.com>
2023-02-13 10:02:54 +0300

    first

---
+++ OOPS/house.py
@@ -0,0 +1,9 @@
+class House1():
+    doors = 4
+    windows = 25
+    kitchen = 2
+    dinning = 2
+    rooms = 16
+
+house1obj = House1()
+print(house1obj.doors,house1obj.windows,house1obj.kitchen,house1obj.dinning,house1obj.rooms)
