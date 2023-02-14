458a3b47fccfb093918184b058fd5a37a098cd65
eMobilis <sandrasasimwa@gmail.com>
2023-02-14 09:36:00 +0300

    quizpython

---
+++ conditions/operators.py
@@ -0,0 +1,9 @@
+for fizzbuzz in range (101):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+        print(fizzbuzz)
+
